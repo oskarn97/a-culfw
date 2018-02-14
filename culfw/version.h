108b69765f2b7e6db7ec376e32cb217897734596
Oskar Neumann <oskar.neumann@me.com>
2018-02-14 14:09:16 +0000

    updated version

--- culfw/version.h
+++ culfw/version.h
@@ -1,6 +1,6 @@
 #define VERSION_1               1
 #define VERSION_2               26
 #define VERSION                 "1.26.02"
-#define BUILD_DATE              "unknown"
-#define BUILD_NUMBER            "private build"
+#define BUILD_DATE              "14.02.2017"
+#define BUILD_NUMBER            "3"
 #define FW_NAME                 "a-culfw"
