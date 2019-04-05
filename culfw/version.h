c801866119341f5dd6169228df07b333da1cec41
Oskar Neumann <oskar.neumann@me.com>
2019-04-05 21:13:19 +0200

    added new binaries

--- culfw/version.h
+++ culfw/version.h
@@ -1,6 +1,6 @@
 #define VERSION_1               1
 #define VERSION_2               26
 #define VERSION                 "1.26.05"
-#define BUILD_DATE              "18.03.2019"
-#define BUILD_NUMBER            "4"
+#define BUILD_DATE              "05.04.2019"
+#define BUILD_NUMBER            "5"
 #define FW_NAME                 "a-culfw"
